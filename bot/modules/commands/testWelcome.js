21785e36457e782ee3a4b551779041e2575bbed2
The Alpha <contact@nabilalsaiad.com>
2023-03-18 07:52:31 +0800

    Adding welcoming to the bot
    The event it self and a command to test

---
+++ bot/modules/commands/testWelcome.js
@@ -0,0 +1,60 @@
+const { ApplicationCommandOptionType } = require("discord.js");
+const {
+	BotCommand,
+	BotCommandDeployment,
+	BotCommandContextMenuType
+} = require("@disqada/halfbot");
+
+const targetCode = "target";
+
+/** @type { import("@disqada/halfbot").BotCommandData } */
+const data = {
+	name: "test-welcome",
+	description: "Tests the welcome event on a member",
+	dmPermission: false,
+	defaultMemberPermissions: ["Administrator"],
+	deployment: BotCommandDeployment.DevGuild,
+	category: "tests",
+	types: {
+		chatInput: true,
+		contextMenu: BotCommandContextMenuType.User
+	},
+	options: [
+		{
+			name: targetCode,
+			description: "The member to welcome",
+			type: ApplicationCommandOptionType.User,
+			required: false
+		}
+	]
+};
+
+/** @param { import("@disqada/halfbot").BotCommandInteraction } interaction */
+async function execute(interaction) {
+	/** @type { import("discord.js").GuildMember } */
+	let target;
+	if (interaction.targetMember) {
+		target = interaction.targetMember;
+	} else {
+		target =
+			interaction.options.getMember(targetCode) ?? interaction.member;
+	}
+
+	if (!target) {
+		throw new Error("No member was provided for testing");
+	}
+
+	const { getFilePath } = require("paths-manager");
+	const welcomeEventFilePath = getFilePath("memberJoin.js");
+
+	/** @type { import("@disqada/halfbot").BotEvent } */
+	const welcomeEvent = require(welcomeEventFilePath);
+
+	welcomeEvent.execute(interaction.bot, target);
+	// return {
+	// 	content: `Welcomed ${target.user.username}`,
+	// 	ephemeral: true
+	// };
+}
+
+module.exports = new BotCommand(data, execute);
---
+++ bot/modules/events/memberJoin.js
@@ -0,0 +1,38 @@
+const { BotEvent } = require("@disqada/halfbot");
+
+/** @type { import("@disqada/halfbot").BotEventData } */
+const data = {
+	name: "guildMemberAdd"
+};
+
+/**
+ * @param { import("@disqada/halfbot").DiscordBot } bot
+ * @param { import("discord.js").GuildMember } member
+ */
+async function execute(bot, member) {
+	const channelId = bot.vars.welcomeChannelId;
+	/** @type { import("discord.js").TextChannel } */
+	const channel = member.guild.channels.cache.get(channelId);
+	if (!channel) {
+		throw new Error("Couldn't find welcoming channel");
+	}
+
+	/** @type { import("discord.js").APIEmbed } */
+	let embed = {
+		title: "أهلا وسهلاً"
+	};
+
+	if (bot.style) {
+		embed = bot.style.applyTo(embed);
+	}
+
+	/** @type { import("discord.js").MessageCreateOptions } */
+	let message = {
+		content: `<@${member.user.id}>`,
+		embeds: [embed]
+	};
+
+	await channel.send(message);
+}
+
+module.exports = new BotEvent(data, execute);
