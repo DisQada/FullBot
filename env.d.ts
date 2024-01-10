0747a84aafd112e8ffcde087281e1b635804cabd
The Alpha <contact@nabilalsaiad.com>
2024-01-10 16:32:33 +0800

    Update workspace

--- env.d.ts
+++ env.d.ts
@@ -1,17 +1,13 @@
 declare global {
-    namespace NodeJS {
-        // Alias for compatibility
-        interface ProcessEnv extends Dict<string> {
-            /**
-             * Can be used to change the default timezone at runtime
-             */
-            TZ?: string;
-            /**
-             * Discord bot token
-             */
-            TOKEN: string;
-        }
+  namespace NodeJS {
+    // Alias for compatibility
+    interface ProcessEnv extends Dict<string> {
+      /**
+       * Discord bot token
+       */
+      TOKEN: string
     }
+  }
 }
 
-export {};
+export {}
